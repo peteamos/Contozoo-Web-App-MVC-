bdd570b88ad0aef26ce8c558a6b9ed020ed81bcf
PeteAmos <peteamos@hotmail.com>
2021-11-02 12:40:04 +0000

    Add project files.

---
+++ Contozoo Web App (MVC)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Contozoo_Web_App__MVC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
